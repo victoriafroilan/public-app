3e14b889758a89106a90028f404782304fab9947
victoriafroilan <froilanrvictoria@gmail.com>
2022-02-10 05:14:45 +0800

    refactor -- base footer layout

--- README.md
+++
@@ -1,3 +0,0 @@
-# nextjs-hq9kcd
-
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nextjs-hq9kcd)
--- components/footer.js
+++ components/footer.js
@@ -1,4 +1,24 @@
 import PropTypes from 'prop-types';
+import Link from 'next/link';
+
+function Tab({ href, title }) {
+  return (
+    <div className="text-neutral-800 dark:text-neutral-200 flex justify-center border-r-2 last:border-x-0 border-neutral-300 dark:border-gray-800">
+      <Link href={href}>
+        <a className="bg-neutral-200 basis-full flex items-center justify-center dark:bg-gray-900">
+          <span className="">
+            <small>{title}</small>
+          </span>
+        </a>
+      </Link>
+    </div>
+  );
+}
+
+Tab.propTypes = {
+  href: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+};
 
 export default function Footer({ name, yearFrom, yearTo }) {
   const years =
@@ -7,15 +27,14 @@ export default function Footer({ name, yearFrom, yearTo }) {
   return (
     <div className className="order-last">
       <div className="h-16 sm:h-7" />
-      <div className="h-16 sm:hidden fixed dark:bg-sky-900 flex inset-x-0 bottom-0">
-        <footer className=" dark:bg-gray-900 dark:text-white justify-center">
-          <div className="">
-            <small>lkjuglkjh</small>
-          </div>
-        </footer>
+      <div className="h-16 sm:hidden fixed dark:bg-sky-900 bg-white grid grid-cols-4 inset-x-0 bottom-0">
+        <Tab href="/link-1" title="link 1" />
+        <Tab href="/link-1" title="link 1" />
+        <Tab href="/link-1" title="link 1" />
+        <Tab href="/link-1" title="link 1" />
       </div>
-      <footer className="hidden sm:flex h-7 fixed bg-slate-900 inset-x-0 bottom-0 justify-center items-center">
-        <div className="text-slate-200">
+      <footer className="hidden sm:flex h-7 fixed dark:bg-gray-900 bg-gray-800 inset-x-0 bottom-0 justify-center items-center">
+        <div className="dark:text-slate-200 text-neutral-100">
           <small>{`Copyright ${years}. ${name}.`}</small>
         </div>
       </footer>
